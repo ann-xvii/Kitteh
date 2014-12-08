d36c841e34cbe14c6211010575f75c9e26de18d4
An.nA <anna.git3@gmail.com>
2014-12-07 19:40:19 -0800

    added seed data :)

--- kitties-starter-app/js/KittiesController.js
+++ kitties-starter-app/js/KittiesController.js
@@ -37,7 +37,49 @@ function KittiesControllerFunc(){
 			gender: 'girl',
 			image: "images/princessmew.png",
 			adopted: false
-		}
+		},
+		{
+			catName: 'Dog',
+			monthsOld: 2,
+			gender: 'boy',
+			image: "https://i.chzbgr.com/maxW500/747434240/h2C883BBD/",
+			adopted: true
+		},
+		{
+			catName: 'Hissy Cat',
+			monthsOld: 6,
+			gender: 'boy',
+			image: "http://www.andrew.cmu.edu/user/cfperron/cats/images/cat8.jpg",
+			adopted: true
+		},
+		{
+			catName: '\'ello Kitty',
+			monthsOld: 17,
+			gender: 'girl',
+			image: "http://2.bp.blogspot.com/-s4e_GXBr5xY/USuOq9LYEaI/AAAAAAAAA-c/BvqQG0HyCzQ/s1600/MundoHelloKitty_Imagen_021.jpg",
+			adopted: true
+		},
+		{
+			catName: 'Frog Cat',
+			monthsOld: 17,
+			gender: 'girl',
+			image: "http://tyrannyoftradition.files.wordpress.com/2012/05/cutest-kitten-hat-ever-13727-1238540322-17.jpg",
+			adopted: true
+		},
+		{
+			catName: 'Cat Stevens',
+			monthsOld: 792,
+			gender: 'boy',
+			image: "http://www.hearnet.com/images_site/Yusuf1.jpg",
+			adopted: false
+		},
+		{
+			catName: 'Larry',
+			monthsOld: 5,
+			gender: 'boy',
+			image: "http://2.bp.blogspot.com/_v7Nd6pidYeQ/S1jR2ZimYgI/AAAAAAAACUc/SuMgsHVZLiE/s400/funny-pictures-kitten-jail.jpg",
+			adopted: false
+		},
 
 	];
 
@@ -64,14 +106,3 @@ function KittiesControllerFunc(){
 		
 	}
 }
-
-
-// SAMPLE PICTURE LINKS: 
-
-// http://2.bp.blogspot.com/-s4e_GXBr5xY/USuOq9LYEaI/AAAAAAAAA-c/BvqQG0HyCzQ/s1600/MundoHelloKitty_Imagen_021.jpg
-
-// http://www.andrew.cmu.edu/user/cfperron/cats/images/cat8.jpg
-
-// https://i.chzbgr.com/maxW500/747434240/h2C883BBD/
-
-// http://2.bp.blogspot.com/_v7Nd6pidYeQ/S1jR2ZimYgI/AAAAAAAACUc/SuMgsHVZLiE/s400/funny-pictures-kitten-jail.jpg
